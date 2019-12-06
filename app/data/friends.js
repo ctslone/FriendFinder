5fda99de7c4ec95381a896d739edce2b9342dd3c
ctslone <ctslone27@gmail.com>
2019-12-06 16:15:01 -0500

    adding photos of the cartoons to the modal. was going to use photos saved in directory but they wont work for some reason. the URL to the images work.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,53 +1,53 @@
 var friendList = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "name": "Spongebob",
+        "photo": "https://cdn1.alternatememories.com/images/intro/TV/xspongebob-squarepants-300x300.png.pagespeed.ic.39isak8s1s.png",
+        "scores": [5, 5, 4, 3, 3, 2, 1, 3, 2, 1]
     },
     {
-        "name": "Sherri",
-        "photo": "http://science-all.com/images/wallpapers/girl-pictures/girl-pictures-23.jpg",
-        "scores": [3, 4, 2, 4, 4, 1, 1, 2, 3, 4]
+        "name": "Patrick",
+        "photo": "https://i.pinimg.com/600x315/1d/4d/b4/1d4db42dc30222dd3be4b1f451319b01.jpg",
+        "scores": [4, 3, 1, 3, 5, 5, 2, 2, 1, 2]
     },
     {
-        "name": "Amy",
-        "photo": "http://www.tenerewilliams.com/wp-content/uploads/2013/02/Naomi-Grossman-headshot.jpg",
-        "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 4]
+        "name": "CatDog",
+        "photo": "https://vignette.wikia.nocookie.net/chroniclesofillusion/images/6/6a/Catdog.png/revision/latest?cb=20180329170609",
+        "scores": [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
     }, 
     {
-        "name": "Gavin",
-        "photo": "https://s-media-cache-ak0.pinimg.com/236x/e6/58/db/e658dbeb3518edeab6a19516b8146137.jpg",
-        "scores": [4, 1, 5, 3, 4, 5, 2, 3, 4, 5]
+        "name": "Sandy",
+        "photo": "https://static.miraheze.org/animatedmusclewomenwiki/thumb/9/9f/SandyCheeksReference.png/300px-SandyCheeksReference.png",
+        "scores": [1, 3, 4, 5, 2, 1, 5, 2, 5, 1]
     }, 
     {
-        "name": "Kayla",
-        "photo": "http://cdn3.thr.com/sites/default/files/2013/02/emma_roberts.jpg",
-        "scores": [4, 1, 5, 3, 4, 5, 2, 3, 4, 5]
+        "name": "Nigel",
+        "photo": "http://media.tumblr.com/tumblr_lnl2yjdS0f1qbm5og.gif",
+        "scores": [2, 3, 2, 5, 1, 3, 5, 4, 3, 2]
     }, 
     {
-        "name": "PJ",
-        "photo": "https://s-media-cache-ak0.pinimg.com/564x/02/72/d2/0272d2536de31958c6dcb3bb1b447586.jpg",
-        "scores": [3, 4, 2, 4, 4, 1, 1, 2, 3, 4]
+        "name": "Otto",
+        "photo": "https://images-na.ssl-images-amazon.com/images/I/41DgOmdNTSL.jpg",
+        "scores": [3, 4, 1, 5, 3, 2, 4, 3, 3, 3]
     }, 
     {
-        "name": "Janie",
-        "photo": "http://kelseyedwardsphoto.com/images/gallery/Sports/74_088_FAVS_MAY_0511.jpg",
-        "scores": [5, 4, 2, 5, 1, 1, 2, 1, 3, 5]
+        "name": "Dexter",
+        "photo": "https://vignette.wikia.nocookie.net/warnerbroscartoons/images/8/8e/Dexter-dexter%27s-laboratory-main-character.png/revision/latest?cb=20160613005414",
+        "scores": [1, 3, 5, 1, 5, 2, 1, 1, 5, 1]
     }, 
     {
-        "name": "Andre",
-        "photo": "http://1.bp.blogspot.com/-6uw9bbJmPs4/VpISNZB5wOI/AAAAAAAAAag/PAQeixYQ4g8/s1600/Varun%2BDhawan%2BLatest%2BPic.jpg",
-        "scores": [1, 5, 1, 3, 5, 1, 4, 3, 2, 1]
+        "name": "Jimmy Neutron",
+        "photo": "https://media.proprofs.com/images/QM/user_images/2165479/1508682184.png",
+        "scores": [5, 5, 4, 4, 4, 2, 5, 2, 4, 1]
     }, 
     {
-        "name": "Krissy",
-        "photo": "http://1.bp.blogspot.com/-FMhfNM7lFG4/Ud8XSJsqgLI/AAAAAAAAAIs/uyMyWoJUEk0/s1600/women-medium-wavy-hairstyles.jpg",
-        "scores": [1, 3, 3, 1, 2, 4, 4, 1, 3, 1]
+        "name": "Carl",
+        "photo": "https://cdn1.alternatememories.com/images/intro/TV/xspongebob-squarepants-300x300.png.pagespeed.ic.39isak8s1s.png",
+        "scores": [5, 3, 3, 1, 5, 5, 1, 1, 1, 5]
     },
     {
-        "name": "Danielle",
-        "photo": "http://benhairstyle.com/wp-content/uploads/2014/10/beautiful_hairstyles_for_young_women.jpg",
-        "scores": [5, 4, 2, 4, 4, 2, 3, 4, 1, 5]
+        "name": "Johnny Bravo",
+        "photo": "https://smallimg.pngkey.com/png/small/51-516121_background-image-johnny-bravo-png.png",
+        "scores": [1, 5, 2, 4, 2, 1, 3, 5, 1, 1]
     },
 ]
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 
 var app= express();
-var PORT = 3000;
+var PORT = 6060;
 
 // send and recieve data through URL
 app.use(express.urlencoded({ extended: true }));
